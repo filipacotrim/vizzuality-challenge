f802e8de505c402a5f8fb1cedc706c3d34dcae94
filipacotrim <filipa.cotrim@tecnico.ulisboa.pt>
2026-02-08 10:36:54 +0100

    Move repo root to include data.json and legend-vizz

---
+++ legend-vizz/app/page.tsx
@@ -0,0 +1,9 @@
+import { Basic } from '../public/components/basic';
+export default function Home() {
+  return (
+      <div>
+        <h1> Setting up!</h1>
+        <Basic />
+      </div>
+  );
+}
