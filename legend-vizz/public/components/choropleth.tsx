d14b4cb83b644ffbb4310723ccc671664a21a4ea
filipacotrim <filipa.cotrim@tecnico.ulisboa.pt>
2026-02-08 22:58:46 +0100

    Choropleth

--- legend-vizz/public/components/choropleth.tsx
+++ legend-vizz/public/components/choropleth.tsx
@@ -1,9 +1,15 @@
 
 export function Choropleth({ item }: { item: any }) {
     return (
-        <div style={{ padding: '1rem', fontSize: '14px', color: '#555' }}>
-            <p>{item.description}</p>
-           
+        <div style={{ display: 'flex', width: '100%'}}>
+            {item.items.map((subItem: any, idx: number) => (
+                <div key={idx} style={{width: `${100 / item.items.length}%`, display: 'flex', flexDirection: 'column',alignItems: 'center'}}>
+                <div style={{ height: '0.5rem', width: '100%', backgroundColor: subItem.color}}/>
+                <p style={{ marginTop: '0.25rem', fontSize: '0.75rem' }}>
+                    {subItem.name}
+                </p>
+                </div>
+            ))}
         </div>
     );
 }
