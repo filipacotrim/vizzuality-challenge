8ec82c39086954d6efd48c1d6260a1aecb238eff
filipacotrim <filipa.cotrim@tecnico.ulisboa.pt>
2026-02-13 23:44:02 +0100

    async fix

--- legend-vizz/app/page.tsx
+++ legend-vizz/app/page.tsx
@@ -1,10 +1,10 @@
 import { LegendWrapper } from '../public/components/legend-wrapper';
 import { LegendItem } from '../public/components/utils';
 
-export default async function Home() {
+export default function Home() {
   //const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/data`, { cache: 'no-store' });
   //const data = await response.json() as LegendItem[];
-
+  console.log("Rendering Home page");
   return (
     <div
       style={{
